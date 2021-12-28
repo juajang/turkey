0701b3fc7846499b0742b0773a49e88cc3e652d5
DOIL KIM <doili0552@gmail.com>
2021-12-28 22:09:44 +0900

    Modify Wallet page

--- src/components/feed/DoughnutChart.js
+++ src/components/feed/DoughnutChart.js
@@ -29,14 +29,14 @@ const DoughnutChart = ({ data, title, style }) => {
           },
         }}
         data={{
-          labels: ["수입", "지출", "투자", "저축"],
+          labels: ["지출", "투자", "저축", "수입"],
           datasets: [
             {
               data,
               backgroundColor: [
-                "rgba(255, 99, 132, 0.2)",
-                "rgba(54, 162, 235, 0.2)",
-                "rgba(255, 206, 86, 0.2)",
+                "#F96666",
+                "#43D0EA",
+                "#FEDD75",
                 "rgba(75, 192, 192, 0.2)",
                 "rgba(153, 102, 255, 0.2)",
                 "rgba(255, 159, 64, 0.2)",
