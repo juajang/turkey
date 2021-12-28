987d757e66e6fc27a5f5d6d591112ab13916377b
DOIL KIM <doili0552@gmail.com>
2021-12-28 22:09:44 +0900

    Modify Wallet page

--- src/pages/Wallet.js
+++ src/pages/Wallet.js
@@ -1,6 +1,7 @@
-import React from "react";
+import React, { useState } from 'react';
 import styled from "styled-components";
-import { Tag, Divider, Button } from 'antd';
+import { Tag, Divider, Button, Collapse, Row, Col, Modal, Form, Input } from 'antd';
+import { DollarOutlined, PlusCircleOutlined } from '@ant-design/icons';
 import DoughnutChart from "../components/feed/DoughnutChart";
 
 const Card = styled.div`
@@ -43,94 +44,253 @@ const Card = styled.div`
   }
 `;
 
+const HeaderWrapper = styled.header`
+  width: 100%;
+  position: relative;
+  height: 80px;
+  font-size: 25px;
+  font-weight: 550;
+  display: grid;
+  place-items: center;
+`;
+
+function priceToString(price) {
+  return price.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',') + "원";
+}
+
 const Wallet = ({ state, dispatch }) => {
   console.log(state);
-  console.log(state.deposit);
-  
-  const { income, consume, invest, deposit } = state;
+  console.log(state.consumeList);
+  const [isConsumeVisible, setIsConsumeVisible] = useState(false);
+  const [isInvestVisible, setIsInvestVisible] = useState(false);
+  const [isDepositVisible, setIsDepositVisible] = useState(false);
+  const [title, setTitle] = useState("");
+  const [price, setPrice] = useState(0);
+  const { income, consume, invest, deposit, consumeList, investList, depositList } = state;
+  const { Panel } = Collapse;
+
+  const consumeOk = () => {
+    setIsConsumeVisible(false);
+    console.log("이름", title);
+    console.log("가격", price);
+    dispatch({ type: "add_consume", title: title, price: parseInt(price)})
+  };
+
+  const consumeCancel = () => {
+    setIsConsumeVisible(false);
+  };
+
+  const investOk = () => {
+    setIsInvestVisible(false);
+    console.log("이름", title);
+    console.log("가격", price);
+    dispatch({ type: "add_invest", title: title, price: parseInt(price)})
+  };
+
+  const investCancel = () => {
+    setIsInvestVisible(false);
+  };
+
+  const depositOk = () => {
+    setIsDepositVisible(false);
+    console.log("이름", title);
+    console.log("가격", price);
+    dispatch({ type: "add_deposit", title: title, price: parseInt(price)})
+  };
+
+  const depositCancel = () => {
+    setIsDepositVisible(false);
+  };
+
+  const Consume = () => {
+    return(
+      <>
+        <div style={{marginRight: "50px"}}>지출</div>
+        <div>{priceToString(consume)}</div>
+      </>
+    )
+  }
+
+  const Invest = () => {
+    return(
+      <>
+        <div style={{marginRight: "50px"}}>투자</div>
+        <div>{priceToString(invest)}</div>
+      </>
+    )
+  }
+
+  const Deposit = () => {
+    return(
+      <>
+        <div style={{marginRight: "50px"}}>저축</div>
+        <div>{priceToString(deposit)}</div>
+      </>
+    )
+  }
 
   return (
     <>
-      <button
-        onClick={() => {
-          dispatch({ type: "income", income: 30 });
-        }}
-      >
-        +30 income
-      </button>
-      <button
-        onClick={() => {
-          dispatch({ type: "income", income: -30 });
-        }}
-      >
-        -30 income
-      </button>
-      <hr></hr>
-      <button
-        onClick={() => {
-          dispatch({ type: "deposit", deposit: 30 });
-        }}
-      >
-        +30 deposit
-      </button>
-      <button
-        onClick={() => {
-          dispatch({ type: "deposit", deposit: -30 });
-        }}
-      >
-        -30 deposit
-      </button>
-      <hr></hr>
-      <button
-        onClick={() => {
-          dispatch({ type: "invest", invest: 30 });
-        }}
-      >
-        +30 invest
-      </button>
-      <button
-        onClick={() => {
-          dispatch({ type: "invest", invest: -30 });
-        }}
-      >
-        -30 invest
-      </button>
+      <HeaderWrapper>
+        <h1>난진</h1>
+      </HeaderWrapper>
       
+      <span className="like" style={{placeItems: "end", float:"right", marginRight:"80px"}}>
+        <DollarOutlined style={{color: "#FBB117", marginRight: "5px"}} />20
+      </span>
+
       <Card>
         <div>
-          <span className="like">$20</span>
           <DoughnutChart
-            data={[deposit, consume, invest]}
+            data={[consume, invest, deposit]}
             style={{
               width: 20,
               height: 20,
             }}
             title={
               <>
-                <div className="nickname">난진</div>
-                <div className="income">{income}/月</div>
+                <div className="income">{priceToString(income)}</div>
               </>
             }
           />
         </div>
       </Card>
+      
+      <Button shape="round" style={{position: "relative", marginTop: "20px"}}>SOL에서 불러오기</Button>
 
-      <Button shape="round">SOL에서 불러오기</Button>
+      <Collapse ghost style={{ postion: "relative", marginTop: "20px" }}>
+        <Panel
+          header={<Consume />}
+          extra={<PlusCircleOutlined
+            style={{ fontSize: "20px", float: "right" }}
+            onClick={(event) => {
+              event.stopPropagation();
+              setIsConsumeVisible(true);
+            }} />} key="201">
+          {consumeList && consumeList
+            .map(({ title, price }) => (
+              <Row>
+                <Col span={1}></Col>
+                <Col span={4}>{"+ ", title}</Col>
+                <Col span={16}>{priceToString(price)}</Col>
+              </Row>
+            ))
+          }
+        </Panel>
+        <Divider />
+        <Panel header={<Invest />} extra={<PlusCircleOutlined
+          style={{ fontSize: "20px", float: "right" }}
+          onClick={(event) => {
+            event.stopPropagation();
+            setIsInvestVisible(true);
+          }} />} key="202">
+          {investList && investList
+            .map(({ title, price }) => (
+              <Row>
+                <Col span={1}></Col>
+                <Col span={4}>{"+ ", title}</Col>
+                <Col span={16}>{priceToString(price)}</Col>
+              </Row>
+            ))
+          }
+        </Panel>
+        <Divider />
+        <Panel header={<Deposit />} extra={<PlusCircleOutlined
+          style={{ fontSize: "20px", float: "right" }}
+          onClick={(event) => {
+            event.stopPropagation();
+            setIsDepositVisible(true);
+          }} />} key="203">
+          {depositList && depositList
+            .map(({ title, price }) => (
+              <Row>
+                <Col span={1}></Col>
+                <Col span={4}>{"+ ", title}</Col>
+                <Col span={16}>{priceToString(price)}</Col>
+              </Row>
+            ))
+          }
+        </Panel>
+      </Collapse>
+      
+      <Modal title="지출 내역 추가" visible={isConsumeVisible} onOk={consumeOk} onCancel={consumeCancel}>
+        <Form>
+          <Form.Item label="지출 이름">
+            <Input
+              placeholder="이름을 입력해주세요"
+              onChange={(event) => {
+                setTitle(event.currentTarget.value);
+              }}
+            />
+          </Form.Item>
+          <Form.Item label="가격">
+            <Input
+              placeholder="가격을 입력해주세요"
+              onChange={(event) => {
+                setPrice(event.currentTarget.value);
+              }}  
+            />
+          </Form.Item>
+        </Form>
+      </Modal>
+
+      <Modal title="투자 내역 추가" visible={isInvestVisible} onOk={investOk} onCancel={investCancel}>
+        <Form>
+          <Form.Item label="투자 이름">
+            <Input
+              placeholder="이름을 입력해주세요"
+              onChange={(event) => {
+                setTitle(event.currentTarget.value);
+              }}
+            />
+          </Form.Item>
+          <Form.Item label="가격">
+            <Input
+              placeholder="가격을 입력해주세요"
+              onChange={(event) => {
+                setPrice(event.currentTarget.value);
+              }}  
+            />
+          </Form.Item>
+        </Form>
+      </Modal>
+
+      <Modal title="저축 내역 추가" visible={isDepositVisible} onOk={depositOk} onCancel={depositCancel}>
+        <Form>
+          <Form.Item label="저축 이름">
+            <Input
+              placeholder="이름을 입력해주세요"
+              onChange={(event) => {
+                setTitle(event.currentTarget.value);
+              }}
+            />
+          </Form.Item>
+          <Form.Item label="가격">
+            <Input
+              placeholder="가격을 입력해주세요"
+              onChange={(event) => {
+                setPrice(event.currentTarget.value);
+              }}  
+            />
+          </Form.Item>
+        </Form>
+      </Modal>
+
       <Divider orientation="left">난진님 특징</Divider>
       <div style={{ marginLeft: "10px" }}>
-        <Tag color="magenta">공격적인 투자자</Tag>
-        <Tag color="red">수익률 50%</Tag>
+        <Tag color="blue">공격적인 투자자</Tag>
+        <Tag color="blue">수익률 50%</Tag>
       </div>
       <Divider orientation="left">난진님 사용상품</Divider>
       <div style={{ marginLeft: "10px" }}>
         <Tag color="cyan">신한 Deep Dream</Tag>
-        <Tag color="blue">반도체주</Tag>
-        <Tag color="green">바이오주</Tag>
+        <Tag color="cyan">반도체주</Tag>
+        <Tag color="cyan">바이오주</Tag>
       </div>
       <Divider orientation="left">난진님과 비슷한 상품 추천</Divider>
       <div style={{ marginLeft: "10px" }}>
-        <Tag color="orange">신한 Deep Dream Platinum+</Tag>
-        <Tag color="purple">배터리주</Tag>
+        <Tag color="green">신한 Deep Dream Platinum+</Tag>
+        <Tag color="green">배터리주</Tag>
       </div>
     </>
   );
--- src/reducer/wallet.js
+++ src/reducer/wallet.js
@@ -1,8 +1,23 @@
 export const initialState = {
-  income: 0, // 수입
-  consume: 0, // 지출
-  invest: 0, // 투자
-  deposit: 0, // 저축
+  income: 9000000, // 수입
+  consume: 1500000, // 지출
+  invest: 3000000, // 투자
+  deposit: 4500000, // 저축
+  consumeList: [
+    {"title": "월세", "price": 800000},
+    {"title": "식비", "price": 500000},
+    {"title": "통신비", "price": 200000},
+  ], // 지출 세부
+  investList: [
+    {"title": "적금", "price": 3000000},
+  ], // 투자 세부
+  depositList: [
+    {"title": "주식", "price": 1000000},
+    {"title": "부동산", "price": 1000000},
+    {"title": "코인", "price": 1000000},
+    {"title": "채권", "price": 1000000},
+    {"title": "펀드", "price": 500000},
+  ], // 저축 세부
 };
 
 export const init = () => initialState;
@@ -10,13 +25,49 @@ export const init = () => initialState;
 export function reducer(state, action) {
   switch (action.type) {
     case "income":
-      return { ...state, income: state.income + action.income };
+      return {
+        ...state,
+        income: state.income + action.income
+      };
     case "consume":
-      return { ...state, consume: state.consume + action.consume };
+      return {
+        ...state,
+        income: state.income + action.consume,
+        consume: state.consume + action.consume
+      };
     case "invest":
-      return { ...state, invest: state.invest + action.invest };
+      return {
+        ...state,
+        income: state.income + action.invest,
+        invest: state.invest + action.invest
+      };
     case "deposit":
-      return { ...state, deposit: state.deposit + action.deposit };
+      return {
+        ...state,
+        income: state.income + action.deposit,
+        deposit: state.deposit + action.deposit
+      };
+    case "add_consume":
+      return {
+        ...state,
+        income: state.income + action.price,
+        consume: state.consume + action.price,
+        consumeList: [...state.consumeList, {"title": action.title, "price": action.price}],
+      };
+    case "add_invest":
+      return {
+        ...state,
+        income: state.income + action.price,
+        invest: state.invest + action.price,
+        investList: [...state.investList, { "title": action.title, "price": action.price }],
+      };
+    case "add_deposit":
+      return {
+        ...state,
+        income: state.income + action.price,
+        deposit: state.deposit + action.price,
+        depositList: [...state.depositList, { "title": action.title, "price": action.price }],
+      };
     default:
       throw new Error();
   }
